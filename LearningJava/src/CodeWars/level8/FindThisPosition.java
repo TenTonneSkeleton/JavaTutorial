190ae4dcf17718002542508ccefabe46ff456787
MurderMostFoul <rasperity@gmail.com>
2022-07-26 17:49:13 +0300

    add CodeWars

---
+++ LearningJava/src/CodeWars/level8/FindThisPosition.java
@@ -0,0 +1,26 @@
+package CodeWars.level8;
+
+public class FindThisPosition {
+    public static String position(char alphabet) {
+        char letter = 'a';
+        int number = 1;
+        if (alphabet == 'a') {
+            return "Position of alphabet: "+number;         //Число которое складывается к строке принимает тип "строка"
+        }
+        while (letter != alphabet) {
+            letter++;
+            number++;
+        }
+        return "Position of alphabet: "+number;
+    }
+
+    public static void main(String[] args) {
+        char alphabet1 = 'a';
+        System.out.println(position(alphabet1));
+        char alphabet2 = 'z';
+        System.out.println(position(alphabet2));
+        char alphabet3 = 'e';
+        System.out.println(position(alphabet3));
+
+    }
+}
