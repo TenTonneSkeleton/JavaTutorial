c2f1506a1a653604d3f449d9915b38bf0fd054a8
TenTonneSkeleton <rasperity@gmail.com>
2022-08-03 00:30:35 +0300

    add isCalendar

--- LearningJava/src/Other/RegularMath.java
+++ LearningJava/src/Other/RegularMath.java
@@ -1,25 +1,26 @@
 package Other;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularMath {
     public static String searchMath (String[] s) {
-        String regexnum = "\\-?\\d+\\.?\\d?";
-        String regexnum2 = "[0-9]";
-        String regexsym = "";
-        String[] result = new String[s.length];
+        String regexNum = "\\s+\\W\\s+";
+        //System.out.println(Arrays.toString(s[0].split(regexNum)));
+       // System.out.println(Arrays.toString(s[1].split(regexNum)));
+        String[][] result = new String[][]{};
+
         for (int i = 0; i < s.length; i++) {
-            Pattern pattern = Pattern.compile(regexnum);
-            Matcher matcher = pattern.matcher(s[i]);
-            if (matcher.find()){
-            System.out.println(matcher.group());
+            for (int j = 0; j < 2; j++) {
+                result[i][j] = s[i].split(regexNum);
             }
-
-        }
+            }
+        System.out.println(result[0][0]);
+        System.out.println(result[0][1]);
         return "Helo";
+        }
 
-    }
 
     public static void main(String[] args) {
         String[] arrayMath = new String[] {"1 + 2", "1.2 *   3.4", "-3  / -6", "-2-2"};
